0fa91909de0a111e973b9816f1fb917bbc492805
Tim Rausch <timrausch@gmx.de>
2017-07-04 18:03:13 +0200

    Built html-structure, roughly styled site-elements, added css-animations

---
+++ src/fx.js
@@ -0,0 +1,16 @@
+$(window).load(function () {
+    /*
+    $('.ticker').marquee({
+    //speed in milliseconds of the marquee
+    duration: 15000,
+    //gap in pixels between the tickers
+    gap: 150,
+    //time in milliseconds before the marquee will start animating
+    delayBeforeStart: 0,
+    //'left' or 'right'
+    direction: 'left',
+    //true or false - should the marquee be duplicated to show an effect of continues flow
+    duplicated: true
+    });
+    */
+});
